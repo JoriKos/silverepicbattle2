61b29595ff8a09209e1534d266dc2aa7763b9d9d
Jorikos <26204@ma-web.nl>
2019-01-27 12:51:14 +0100

    class creation

---
+++ src/Main.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Kies het aantal spelers (Minimaal 1 en maximaal 14):");
+        Scanner s = new Scanner(System.in);
+
+        int y = s.nextInt();
+
+        if(y == 1 || y == 0){
+            Player a = new Player();
+        } else if(y == 2){
+            Player a = new Player();
+            Player b = new Player();
+        } else if(y == 3){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+        } else if(y == 4){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+        } else if(y == 5){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+        } else if(y == 6){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+            Player f = new Player();
+        } else if(y == 7) {
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+            Player f = new Player();
+            Player g = new Player();
+        } else if(y == 8){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+            Player f = new Player();
+            Player g = new Player();
+            Player h = new Player();
+        } else if(y == 9){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+            Player f = new Player();
+            Player g = new Player();
+            Player h = new Player();
+            Player i = new Player();
+        } else if(y == 10){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+            Player f = new Player();
+            Player g = new Player();
+            Player h = new Player();
+            Player i = new Player();
+            Player j = new Player();
+        } else if(y == 11){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+            Player f = new Player();
+            Player g = new Player();
+            Player h = new Player();
+            Player i = new Player();
+            Player j = new Player();
+            Player k = new Player();
+        } else if(y == 12){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+            Player f = new Player();
+            Player g = new Player();
+            Player h = new Player();
+            Player i = new Player();
+            Player j = new Player();
+            Player k = new Player();
+            Player l = new Player();
+        } else if(y == 13){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+            Player f = new Player();
+            Player g = new Player();
+            Player h = new Player();
+            Player i = new Player();
+            Player j = new Player();
+            Player k = new Player();
+            Player l = new Player();
+            Player m = new Player();
+        } else if(y >= 14){
+            Player a = new Player();
+            Player b = new Player();
+            Player c = new Player();
+            Player d = new Player();
+            Player e = new Player();
+            Player f = new Player();
+            Player g = new Player();
+            Player h = new Player();
+            Player i = new Player();
+            Player j = new Player();
+            Player k = new Player();
+            Player l = new Player();
+            Player m = new Player();
+            Player n = new Player();
+        }
+    }
+}
